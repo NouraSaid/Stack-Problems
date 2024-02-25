a768e784b2ea92791e0d8a7ba73a67c1ad4312a5
NouraSaid <noura.alsaidi98@gmail.com>
2024-02-25 09:52:33 +0400

    Print element from a stack

---
+++ printStack.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class printStack {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(12);
+        stack.push(13);
+        stack.push(14);
+        stack.push(15);
+       
+         
+        System.out.println(stack);
+
+    }
+}
