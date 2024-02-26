e716368669c8d9944beaef08ae62c673997a50a9
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-26 09:23:05 +0400

    Update printStack.java

--- printStack.java
+++ printStack.java
@@ -3,15 +3,15 @@ import java.util.Stack;
 public class PrintStack {
 
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>(); //add new stacl with integer data type
         
-        stack.push(12);
+        stack.push(12); //push number in the stack
         stack.push(13);
         stack.push(14);
         stack.push(15);
        
          
-        System.out.println(stack);
+        System.out.println(stack); //print the elements 
 
     }
 }
