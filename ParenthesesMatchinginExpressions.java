4675b79681771fdb360c855073cd7869a408226a
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-26 13:21:45 +0400

    Add files via upload

---
+++ ParenthesesMatchinginExpressions.java
@@ -0,0 +1,23 @@
+ public static boolean checkValidParenthesis(String str) {
+        if (str.isEmpty())
+            return true;
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            if (current == '{' || current == '(' || current == '[') {
+                stack.push(current);
+                continue;
+            }
+            if (current == '}' || current == ')' || current == ']') {
+                if (stack.isEmpty())
+                    return false;
+
+                char last = stack.peek();
+                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
